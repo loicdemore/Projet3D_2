62fe2dadfc243d0b5f8a0a4b67d0616de7e54912
RemiPomme <remi.pomme@polytechnique.edu>
2024-12-30 12:23:30 +0100

    1st attempt task 3

--- Codes/Registration.py
+++ Codes/Registration.py
@@ -1,6 +1,6 @@
 import SimpleITK as sitk
 
-def est_lin_transf(im_ref, im_mov):
+def est_lin_transf(im_ref, im_mov, mask = None, verbose = False):
     """
     Estimate linear (affine) transform to align `im_mov` to `im_ref`
     and return the transform parameters.
@@ -13,18 +13,31 @@ def est_lin_transf(im_ref, im_mov):
     registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
     registration_method.SetMetricSamplingPercentage(0.01)  # Use a small percentage of pixels for speed
     
+    if mask is not None:
+        registration_method.SetMetricFixedMask(mask)
+        
     # Interpolation
     registration_method.SetInterpolator(sitk.sitkLinear)
     
     # Optimizer settings
-    registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100, convergenceMinimumValue=1e-6, convergenceWindowSize=10)
+    registration_method.SetOptimizerAsGradientDescent(learningRate=1, numberOfIterations=500, convergenceMinimumValue=1e-3, convergenceWindowSize=20)
+    registration_method.SetOptimizerScalesFromPhysicalShift()
     
     # Transformation model
     initial_transform = sitk.CenteredTransformInitializer(im_ref, im_mov, sitk.AffineTransform(im_ref.GetDimension()), sitk.CenteredTransformInitializerFilter.GEOMETRY)
     registration_method.SetInitialTransform(initial_transform, inPlace=False)
     
     # Perform registration
-    final_transform = registration_method.Execute(im_ref, im_mov)
+    final_transform = registration_method.Execute(sitk.Cast(im_ref, sitk.sitkFloat32), 
+                                                  sitk.Cast(im_mov, sitk.sitkFloat32))
+
+    if verbose:
+        print("--------")
+        print("Affine registration:")
+        print(f'Final metric value: {registration_method.GetMetricValue()}')
+        print(f"Optimizer stop condition: {registration_method.GetOptimizerStopConditionDescription()}")
+        print(f"Number of iterations: {registration_method.GetOptimizerIteration()}")
+        print("--------")
     
     return final_transform
 
